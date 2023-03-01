dd31bdd46e7beef090180b6c7132f2c485adada4
Richa Dwivedi <richa_dwivedi@mckinsey.com>
2023-03-01 05:30:11 +0000

    all changes

--- srv/product-details-srv.cds
+++ srv/product-details-srv.cds
@@ -3,5 +3,6 @@ using {product.details as my} from '../db/product-details';
 service ProductService {
 
     entity Products as projection on my.Products;
-    function top_10_rows() returns Boolean;
+    function top_10_rows() returns array of Products;
+    // function top_10_rows() returns Boolean;
 }
--- srv/product-details-srv.js
+++ srv/product-details-srv.js
@@ -1,32 +1,25 @@
 const cds = require('@sap/cds')
+const hdbext = require("@sap/hdbext")
+const dbClass = require("sap-hdbext-promisfied")
 module.exports = cds.service.impl(function () {
 
     this.on('top_10_rows', async () => {
         try {
-            let dbQuery = ' Call "top_10_rows"( )';
-            let result = await cds.run(dbQuery, {});
-            console.log(result);
-            return true;
+            const db = await cds.connect.to('db')
+
+            // using @sap/hdbext module to call the Stored Procedure from HANA.
+            let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials))
+            const sp = await dbConn.loadProcedurePromisified(hdbext, null, 'top_10_rows')
+            const output = await dbConn.callProcedurePromisified(sp, [])
+            console.log(output.results)
+            return output.results
         } catch (error) {
-            console.error(error)
-            return false
+            console.error(error);
+            return [];
         }
     })
+
 })
 
-
-// const cds = require('@sap/cds')
-// module.exports = cds.service.impl(async (srv) => {
-//     const db = await cds.connect.to("db");
-//     const { Products } = cds.entities('product.details');
-//     srv.on("top_10_rows", async () => {
-//         var list = await srv.run(SELECT.from(Products));
-
-//         let query=' Call "top_10_rows"()';
-//         result = await srv.run(query,{});
-//         console.log(result);
-//         return list;
-//     })
-// })
 
 
